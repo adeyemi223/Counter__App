bce9579f684841077a990457c057913ee0064d2b
Falujo Adeyemi <falujoadeyemi6776@gmail.com>
2024-01-09 14:27:15 +0100

    new html && css added

---
+++ index.js
@@ -0,0 +1,16 @@
+// let count = 0;
+
+// document.getElementById("Increment").onclick = function(){
+//     count+= 1;
+//     document.getElementById("countinput").innerHTML = count;
+// }
+
+// document.getElementById("Decrement").onclick = function(){
+//     count-= 1;
+//     document.getElementById("countinput").innerHTML = count;
+// }
+
+// document.getElementById("Reset").onclick = function(){
+//     count = 0;
+//     document.getElementById("countinput").innerHTML = count;
+// }
